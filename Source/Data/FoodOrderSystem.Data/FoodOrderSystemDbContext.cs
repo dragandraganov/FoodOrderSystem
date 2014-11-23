e66f37ab5bdc478d1ddc9b1d20b77fedcebb8620
Dr4g0 <d_d0003@yahoo.com>
2014-11-23 12:33:11 +0200

    Moved User model and DbContext in relevant projects

---
+++ Source/Data/FoodOrderSystem.Data/FoodOrderSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace FoodOrderSystem.Data
+{
+    using System;
+    using System.Linq;
+    using FoodOrderSystem.Data.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class FoodOrderSystemDbContext : IdentityDbContext<User>
+    {
+        public FoodOrderSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static FoodOrderSystemDbContext Create()
+        {
+            return new FoodOrderSystemDbContext();
+        }
+    }
+}
--- Source/Web/FoodOrderSystem.Web/Controllers/AccountController.cs
+++ Source/Web/FoodOrderSystem.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using FoodOrderSystem.Web.Models;
+using FoodOrderSystem.Data.Models;
 
 namespace FoodOrderSystem.Web.Controllers
 {
@@ -151,7 +152,7 @@ namespace FoodOrderSystem.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +368,7 @@ namespace FoodOrderSystem.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new User { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
