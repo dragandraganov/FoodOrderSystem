3b2be47cc57409a543d0e015c607f0f7ed4653be
Dr4g0 <d_d0003@yahoo.com>
2014-12-15 14:42:43 +0200

    Integrated AutoMapper with FoodOrderSystem.Web, added library FoodOrderSystem.Web.Infrastructure

---
+++ Source/Web/FoodOrderSystem.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodOrderSystem.Web.Infrastructure.Mapping
+{
+    using AutoMapper;
+ 
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
